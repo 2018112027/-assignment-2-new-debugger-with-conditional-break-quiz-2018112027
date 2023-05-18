16aec5e16b5650fa711def99d4ff8a75267b8141
조현수 <129857005+2018112027@users.noreply.github.com>
2023-05-18 18:25:03 +0900

    Update CondBreakDebugger.py

--- CondBreakDebugger.py
+++ CondBreakDebugger.py
@@ -1,9 +1,15 @@
 from utils import input, next_inputs
 import sys
 import ast
+from typing import Any, Dict, TextIO
 from debuggingbook.Debugger import Debugger
 
 class CondBreakDebugger(Debugger):
+    def __init__(self, *, file: TextIO = sys.stdout) -> None:
+        self.breakvars: Dict[str, Any] = {}
+        super().__init__(file = file)
+
+class CondBreakDebugger(CondBreakDebugger):
     def interaction_loop(self) -> None:
         """Interact with the user"""
         self.print_debugger_status(self.frame, self.event, self.arg)  # type: ignore
@@ -12,7 +18,6 @@ class CondBreakDebugger(Debugger):
         while self.interact:
             command = input("(debugger) ")
             self.execute(command)  # type: ignore
-
 
 class CondBreakDebugger(CondBreakDebugger):
     def attr_command(self, arg: str = "") -> None:
@@ -24,7 +29,6 @@ class CondBreakDebugger(CondBreakDebugger):
         value = eval(EXPR, OBJ.__dict__)
         setattr(OBJ, VAR, value)
         print(f"Set {VAR} to {value} for object {OBJ}")
-
 
 class CondBreakDebugger(CondBreakDebugger):
     def set_command(self, arg: str = "") -> None:
@@ -38,7 +42,21 @@ class CondBreakDebugger(CondBreakDebugger):
         except Exception as err:
             self.log(f"{err.__class__.__name__}: {err}")
 
+class CondBreakDebugger(CondBreakDebugger):
+    def break_command(self, arg: str = "") -> None:
+        token = arg.split()
+        if len(token) == 1:
+            self.breakpoints.add(int(arg))
+        elif len(token) == 3:
+            var = token[0].strip()
+            value = token[2].strip()
+            self.breakvars[var] = eval(value, self.frame.f_globals, self.breakvars)
 
-
-
-
+class CondBreakDebugger(CondBreakDebugger):
+    def stop_here(self) -> bool:
+        """Return True if we should stop"""
+        for var, value in self.breakvars:
+            if var in self.local_vars:
+                if self.breakvars[var] == value:
+                    return True
+        return self.stepping or self.frame.f_lineno in self.breakpoints
