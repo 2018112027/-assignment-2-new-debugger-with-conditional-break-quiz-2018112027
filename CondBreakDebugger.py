53be015564d84b6f84e8ccd5691973f1a52e20e8
조현수 <129857005+2018112027@users.noreply.github.com>
2023-05-18 13:24:25 +0900

    Update CondBreakDebugger.py

--- CondBreakDebugger.py
+++ CondBreakDebugger.py
@@ -1,19 +1,21 @@
 from utils import next_inputs, input
-import bookutils
 import sys
-from Debugger import Debugger
+import ast
+from debuggingbook.Debugger import Debugger
 
 class CondBreakDebugger(Debugger):
     def set_command(self, arg: str = "") -> None:
-    temp = arg.split(' ')
-    var = temp[0]
-    value = temp[2] 
-    self.frame.f_locals[var] = value
-class cbDebugger(Debugger):
-    def attr_command(self, arg: str = "") -> None:
-        temp = str.split(' ')
-        OBJ = temp[0]
-        VAR = temp[1]
-        EXPR = temp[2] 
+        token = arg.split('=')
+        var = token[0].strip()
+        value = token[1].strip()
         
-    
+        try:
+            value = ast.literal_eval(value)
+        except (ValueError, SyntaxError):
+            pass
+            
+        if var in self.local_vars:
+            self.frame.f_locals[var] = value
+            print(f"Set {var} to {value}")
+        else:
+            print(f"Variable {var} not found")
